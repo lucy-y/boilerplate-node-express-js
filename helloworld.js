3cf0799475ae012ddd2bdbb6a554807a1bcdd382
lucy <>
2022-12-12 11:08:22 +0900

    routing

--- helloworld.js
+++ helloworld.js
@@ -2,8 +2,21 @@ const http = require('http')
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello world')
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+
+    switch(path){
+        case '':
+            res.writeHead(200, {'Content-Type': 'text/plain'})
+            res.end('Home')
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'})
+            res.end('About')
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'})
+            res.end('Not Found')
+            break;
+    }
+    
 })
 
 server.listen(port, () => console.log(`Server started on port ${port}; `))
