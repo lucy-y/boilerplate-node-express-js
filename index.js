b987ac6dfee563c54d9db0e8466644149477903c
lucy <>
2022-12-12 17:11:34 +0900

    eslint 추가 및 예외처리

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require('express')
 const app = express()
 const expressHandlebars = require('express-handlebars')
+const { request } = require('http')
+/* eslint-disable no-undef */
 const port = process.env.PORT || 3000
+/* eslint-disable no-undef */
 
-const fortune = require('./public/lib/fortune')
 const handlers = require('./public/lib/handlers')
 
 app.engine('handlebars', expressHandlebars({
     defalutLayout: 'main'
 }))
 
+/* eslint-disable no-undef */
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
+/* eslint-disable no-undef */
 
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
 app.use(handlers.notFound)
 app.use(handlers.serverError)
 
-app.listen(port, () => {
-    `Server started on port ${port}; `
-})
+if(request.main === module){
+    app.listen(port, () => {
+        `Server started on port ${port}; `
+    })
+} else {
+    module.exports = app
+}
