bfd9bcd00b914e0b48cbcb7ec62ccf3da93e2229
lucy <>
2022-12-12 16:20:39 +0900

    test jest 예시 추가

--- public/lib/__test__/handler.test.js
+++ public/lib/__test__/handler.test.js
@@ -7,3 +7,32 @@ test('home page renders', () => {
     expect(res.render.mock.calls.length).toBe(1)
     expect(res.render.mock.calls[0][0]).toBe('home')
 })
+
+test('about page renders', () => {
+    const req = {}
+    const res = { render: jest.fn() }
+    handlers.about(req, res)
+    expect(res.render.mock.calls.length).toBe(1)
+    expect(res.render.mock.calls[0][0]).toBe('about')
+    expect(res.render.mock.calls[0][1]).toEqual(expect.objectContaining({
+        fortune: expect.stringMatching(/^message[0-5]$/)
+    }))
+})
+
+test('404 page renders', () => {
+    const req = {}
+    const res = { render: jest.fn() }
+    handlers.notFound(req, res)
+    expect(res.render.mock.calls.length).toBe(1)
+    expect(res.render.mock.calls[0][0]).toBe('404')
+})
+
+test('500 page renders', () => {
+    const err = new Error('some error')
+    const req = {}
+    const res = { render: jest.fn() }
+    const next = jest.fn()
+    handlers.serverError(err, req, res, next)
+    expect(res.render.mock.calls.length).toBe(1)
+    expect(res.render.mock.calls[0][0]).toBe('500')
+})
